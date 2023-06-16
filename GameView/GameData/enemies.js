b4dbdca8a5bf057a785b9e7b563e01382fe0df22
David Palomino <david.leandro.palomino.rivera@gmail.com>
2023-06-16 00:06:29 -0500

    Layout Finish

---
+++ GameView/GameData/enemies.js
@@ -0,0 +1,10 @@
+class Enemies{
+    constructor(life, armor, damage, drop){
+        this.life = life
+        this.armor = armor
+        this.damage = damage
+        this.drop = damage
+    }
+}
+
+
